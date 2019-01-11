ab3ed6dcee068a3fa3af29d7f800532be6e81e3e
Jonas Karlman <jonas@kwiboo.se>
2019-01-11 21:30:57 +0100

    restore 10.6 api

--- driver/product/kernel/drivers/gpu/arm/midgard/mali_kbase_config.h
+++ driver/product/kernel/drivers/gpu/arm/midgard/mali_kbase_config.h
@@ -216,6 +216,20 @@ struct kbase_pm_callback_conf {
 	int (*soft_reset_callback)(struct kbase_device *kbdev);
 };
 
+/**
+ * kbase_gpu_clk_speed_func - Type of the function pointer for GPU_SPEED_FUNC
+ * @param clock_speed - pointer to store the current GPU clock speed in MHz
+ *
+ * Returns 0 on success, otherwise negative error code.
+ * When an error is returned the caller assumes maximum GPU speed stored in
+ * gpu_freq_khz_max.
+ *
+ * If the system timer is not available then this function is required
+ * for the OpenCL queue profiling to return correct timing information.
+ *
+ */
+typedef int (*kbase_gpu_clk_speed_func) (u32 *clock_speed);
+
 #ifdef CONFIG_OF
 struct kbase_platform_config {
 };
